057b5aa596afad4e8cf1a3b441117a9a9cd20dcd
fell30 <rafaelsimarmata01@gmail.com>
2025-07-28 07:20:21 +0700

    mencoba EnemyHealth

---
+++ Assets/Script/Enemy/EnemySystem/EnemyHealth.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [Header("Enemy Health Settings")]
+    public float maxHealth = 100f;
+    private float currentHealth;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    
+    public void TakeDamage(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+        Debug.Log($"{gameObject.name} took {damageAmount} damage. Remaining health: {currentHealth}");
+
+        if (currentHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log($"{gameObject.name} has died.");
+        Destroy(gameObject); 
+    }
+}
