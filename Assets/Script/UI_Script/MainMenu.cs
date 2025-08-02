2423feda65b4e6a90317c9e37720b34c2f78d43d
Rashelyo123 <rashelyosyawalian31@gmail.com>
2025-08-03 01:19:57 +0700

    ghh

--- Assets/Script/UI_Script/MainMenu.cs
+++ Assets/Script/UI_Script/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
+    private string selectedLevelName;
     public void EnableMenu(GameObject menu)
     {
         menu.SetActive(true);
@@ -18,6 +18,23 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(sceneName);
         Time.timeScale = 1f;
+    }
+    public void SelectLevel(string levelName)
+    {
+        selectedLevelName = levelName;
+        Debug.Log("Level selected: " + selectedLevelName);
+    }
+
+    public void PlaySelectedLevel()
+    {
+        if (!string.IsNullOrEmpty(selectedLevelName))
+        {
+            SceneManager.LoadScene(selectedLevelName);
+        }
+        else
+        {
+            Debug.LogWarning("No level selected!");
+        }
     }
     public void QuitGame()
     {
