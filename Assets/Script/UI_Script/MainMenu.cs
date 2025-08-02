2aacb57e78145e975fecaf8473df03b457855f05
Rashelyo123 <rashelyosyawalian31@gmail.com>
2025-08-03 02:03:43 +0700

    ASas

--- Assets/Script/UI_Script/MainMenu.cs
+++ Assets/Script/UI_Script/MainMenu.cs
@@ -1,11 +1,16 @@
 using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
     private string selectedLevelName;
+    public TextMeshProUGUI percentText;
+    public Slider LoadingScreen;
+
     public void EnableMenu(GameObject menu)
     {
         menu.SetActive(true);
@@ -29,12 +34,52 @@ public class MainMenu : MonoBehaviour
     {
         if (!string.IsNullOrEmpty(selectedLevelName))
         {
-            SceneManager.LoadScene(selectedLevelName);
+            StartCoroutine(LoadLevelAsync(selectedLevelName));
+
         }
         else
         {
-            Debug.LogWarning("No level selected!");
+            StartCoroutine(LoadLevelAsync("Level_1"));
+
         }
+    }
+    IEnumerator LoadLevelAsync(string levelName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelName);
+        operation.allowSceneActivation = false;
+
+        float timer = 0f;
+
+        while (operation.progress < 0.9f)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            if (LoadingScreen != null)
+                LoadingScreen.value = progress;
+
+            if (percentText != null)
+                percentText.text = "Loading... " + Mathf.RoundToInt(progress * 100f) + "%";
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        // Pastikan loading minimal 2 detik
+        while (timer < 2f)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        // Finalize loading bar
+        if (LoadingScreen != null)
+            LoadingScreen.value = 1f;
+
+        if (percentText != null)
+            percentText.text = "Loading... 100%";
+
+        yield return new WaitForSeconds(0.5f);
+        operation.allowSceneActivation = true;
     }
     public void QuitGame()
     {
